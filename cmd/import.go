9f47917db51d05e4bbf6cd93f57314d525d49e05
Jonas Vinther <jrvinther@gmail.com>
2020-11-25 22:51:20 +0100

    Read flags from env file

--- cmd/import.go
+++ cmd/import.go
@@ -5,6 +5,7 @@ import (
 	"medusa/pkg/vaultengine"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 func init() {
@@ -18,6 +19,21 @@ var importCmd = &cobra.Command{
 	Args:  cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		file := args[0]
+
+		// Apply the viper config value to the flag when the flag is not set and viper has a value
+		if viper.IsSet("VAULT_ADDR") {
+			value := viper.Get("VAULT_ADDR").(string)
+			cmd.Flags().Set("vault-url", value)
+		}
+		if viper.IsSet("VAULT_TOKEN") {
+			value := viper.Get("VAULT_TOKEN").(string)
+			cmd.Flags().Set("vault-token", value)
+		}
+		if viper.IsSet("VAULT_TOKEN") {
+			value := viper.Get("VAULT_TOKEN").(string)
+			cmd.Flags().Set("vault-token", value)
+		}
+
 		vaultURL, _ := cmd.Flags().GetString("vault-url")
 		vaultToken, _ := cmd.Flags().GetString("vault-token")
 		vaultPrefix, _ := cmd.Flags().GetString("vault-prefix")
--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/google/go-cmp v0.3.0
 	github.com/hashicorp/vault/api v1.0.4
 	github.com/spf13/cobra v1.1.1
+	github.com/spf13/viper v1.7.0
 	gopkg.in/yaml.v2 v2.3.0
 )
