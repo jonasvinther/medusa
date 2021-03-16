7457fd61183e7881802661d95e30ac336a7d34ac
Jonas Vinther <jrvinther@gmail.com>
2021-03-16 21:44:59 +0100

    Update main.go (#33)
    Clean up main.go

--- main.go
+++ main.go
@@ -12,26 +12,4 @@ func main() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-
-	// key := encrypt.NewAesEncryptionKey()
-	// text := []byte("Hello from Medusa")
-	// encrypted, err := encrypt.AesEncrypt(text, key)
-
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-
-	// fmt.Println("Text: " + string(text))
-	// fmt.Println("Encrypted: " + string(encrypted))
-
-	// privKey := encrypt.ReadRsaPrivateKey("./private-key.pem")
-	// pubKey := encrypt.ReadRsaPublicKey("./public-key.pem")
-
-	// encrptedRsa, _ := encrypt.RsaEncrypt(key, pubKey)
-	// decryptedKey, _ := encrypt.RsaDecrypt(encrptedRsa, privKey)
-
-	// decrypted, err := encrypt.AesDecrypt(encrypted, decryptedKey)
-	// fmt.Println("Decrypted: " + string(decrypted))
-
-	// fmt.Println("encrypt: " + encrptedRsa)
 }
