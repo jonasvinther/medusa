6f8749eba69cc1653e28d7569320363a3245e6d3
Jonas Vinther <jrvinther@gmail.com>
2021-10-09 20:12:55 +0200

    Testing

--- pkg/vaultengine/secret_write.go
+++ pkg/vaultengine/secret_write.go
@@ -33,11 +33,12 @@ func (client *Client) SecretWrite(path string, data map[string]interface{}) {
 			fmt.Printf("Secret successfully written to Vault [%s] using path [%s]\n", client.addr, path)
 		}
 	} else {
+		fmt.Printf("No json: %+v \n", data)
 		_, err := client.vc.Logical().Write(finalPath, finalData)
 		if err != nil {
-			fmt.Printf("Error while writing secret. %s\n", err)
+			fmt.Printf("Error while writing secret. %s\n\n", err)
 		} else {
-			fmt.Printf("Secret successfully written to Vault [%s] using path [%s]\n", client.addr, path)
+			fmt.Printf("Secret successfully written to Vault [%s] using path [%s]\n\n", client.addr, path)
 		}
 	}
 }
