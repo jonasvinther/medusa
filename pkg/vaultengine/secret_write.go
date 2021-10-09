05f1299bb95ec4a7c7b6cc1944a91c33937806bb
Jonas Vinther <jrvinther@gmail.com>
2021-10-09 20:04:38 +0200

    add writebytes func

--- pkg/vaultengine/secret_write.go
+++ pkg/vaultengine/secret_write.go
@@ -1,6 +1,9 @@
 package vaultengine
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
 
 // SecretWrite is used for writing data to a Vault instance
 func (client *Client) SecretWrite(path string, data map[string]interface{}) {
@@ -20,10 +23,21 @@ func (client *Client) SecretWrite(path string, data map[string]interface{}) {
 		finalData["data"] = data
 	}
 
-	_, err := client.vc.Logical().Write(finalPath, finalData)
-	if err != nil {
-		fmt.Printf("Error while writing secret. %s\n", err)
+	if val, ok := data["json-key"]; ok {
+		fmt.Println("Do json", val)
+		b, err := json.Marshal([]byte(val.(string)))
+		_, err = client.vc.Logical().WriteBytes(finalPath, b)
+		if err != nil {
+			fmt.Printf("Error while writing secret. %s\n", err)
+		} else {
+			fmt.Printf("Secret successfully written to Vault [%s] using path [%s]\n", client.addr, path)
+		}
 	} else {
-		fmt.Printf("Secret successfully written to Vault [%s] using path [%s]\n", client.addr, path)
+		_, err := client.vc.Logical().Write(finalPath, finalData)
+		if err != nil {
+			fmt.Printf("Error while writing secret. %s\n", err)
+		} else {
+			fmt.Printf("Secret successfully written to Vault [%s] using path [%s]\n", client.addr, path)
+		}
 	}
 }
