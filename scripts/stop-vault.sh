13f86fd32cb9e0c64d5581f16cb52287f416aaac
Jonas Vinther <jorv@lunar.app>
2024-12-16 12:20:08 +0100

    Support docker compose v2

--- scripts/stop-vault.sh
+++ scripts/stop-vault.sh
@@ -4,4 +4,4 @@
 echo "Stopping Vault container"
 export SCRIPT_SOURCE=$(dirname "${BASH_SOURCE[0]}")
 (cd $SCRIPT_SOURCE
-docker-compose -f docker-compose-tls.yml down)
+docker compose -f docker-compose-tls.yml down)
